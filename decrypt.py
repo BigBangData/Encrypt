d39641f550e1a61fcb770c985b913ebcda5f5fe1
Marcelo Sanches <sanches.cello@gmail.com>
2024-05-02 19:19:32 -0600

    Fix decrypt

--- decrypt.py
+++ decrypt.py
@@ -22,18 +22,18 @@ def decrypt_file(input_file, output_file, password):
     subprocess.run(command, check=True)
 
 def main():
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 3:
         print("Usage: python decrypt.py <folder_name> <password>")
         sys.exit(1)
 
-    folder_name = sys.argv[1]
+    folder_path = sys.argv[1]
     password = sys.argv[2]
 
     # Decryption
-    decrypt_file(folder_name + ".enc", folder_name + ".tar.gz", password)
-    os.remove(folder_name + ".enc")
-    shutil.unpack_archive(folder_name + ".tar.gz", folder_name)
-    os.remove(folder_name + ".tar.gz")
+    decrypt_file(folder_path + ".enc", folder_path + ".tar.gz", password)
+    os.remove(folder_path + ".enc")
+    shutil.unpack_archive(folder_path + ".tar.gz", folder_path)
+    os.remove(folder_path + ".tar.gz")
 
 if __name__ == "__main__":
     main()
